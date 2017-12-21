d723d382420110ca0942e18c5fbd6e7bcc341a2d
kityara101 <kityar@mail.ru>
2017-12-21 18:39:37 +0300

    orderer and ca are now deploing to bluemix

--- 3org-2chcodes/bluemix/deploy-org.sh
+++ 3org-2chcodes/bluemix/deploy-org.sh
@@ -1,15 +1,33 @@
 #!/bin/sh -e
 
-#kubectl create  -f ./brand1/brand1-volumes.yml
+cd tracelabel
 
-#kubectl create secret generic brand1-peer-db --from-file=user=./brand1/.db.username --from-file=password=./brand1/.db.password
+#kubectl create  -f ./volumes.yml
 
-#kubectl create  -f ./brand1/brand1-services-couchdb.yml
 
-kubectl create secret generic brand1-ca  --from-file=ca-key=../crypto-config/peerOrganizations/brand1.com/ca/9dfd37cc13ad2315800f4a3bf1f60fda81f005b3338f68a59585b29e98165a46_sk \
-                                        --from-file=ca-cert=../crypto-config/peerOrganizations/brand1.com/ca/ca.brand1.com-cert.pem  \
+
+
+CA_TRACELABEL_PRIVATE_KEY=$(ls -f1 ../../crypto-config/ordererOrganizations/tracelabel.com/ca | grep _sk)
+#kubectl create secret generic tracelabel-ca  --from-file=../../crypto-config/peerOrganizations/tracelabel.com/ca/$CA_TRACELABEL_PRIVATE_KEY \
+#                                       --from-file=../../crypto-config/peerOrganizations/tracelabel.com/ca/ca.tracelabel.com-cert.pem  
  
-kubectl create secret generic brand1-msp  --from-file=admin-cert=../crypto-config/peerOrganizations/brand1.com/msp/admincerts/Admin@brand1.com-cert.pem \
-                                        --from-file=ca-cert=../crypto-config/peerOrganizations/brand1.com/msp/cacerts/ca.brand1.com-cert.pem  
+#kubectl create secret generic tracelabel-msp  --from-file=admin-cert=../../crypto-config/peerOrganizations/tracelabel.com/msp/admincerts/Admin@tracelabel.com-cert.pem \
+#                                        --from-file=ca-cert=../../crypto-config/peerOrganizations/tracelabel.com/msp/cacerts/ca.tracelabel.com-cert.pem  
 
+#kubectl create  -f ./services-ca.yml
+
+
+#kubectl create secret generic orderer-block  --from-file=../../config/genesis.block
+kubectl create secret generic orderer-msp   --from-file=admin_cert=../../crypto-config/ordererOrganizations/tracelabel.com/orderers/orderer.tracelabel.com/msp/admincerts/Admin@tracelabel.com-cert.pem \
+                                            --from-file=../../crypto-config/ordererOrganizations/tracelabel.com/orderers/orderer.tracelabel.com/msp/cacerts \
+                                            --from-file=../../crypto-config/ordererOrganizations/tracelabel.com/orderers/orderer.tracelabel.com/msp/keystore \
+                                            --from-file=../../crypto-config/ordererOrganizations/tracelabel.com/orderers/orderer.tracelabel.com/msp/signcerts \
+                                            --from-file=../../crypto-config/ordererOrganizations/tracelabel.com/orderers/orderer.tracelabel.com/msp/tlscacerts
+
+#kubectl create -f tracelabel/services-orderer.yml 
+
+#kubectl create secret generic tracelabel-peer-db --from-file=user=.db.username --from-file=password=.db.password
+
+
+#kubectl create  -f ./services-couchdb.yml
 #kubectl create  -f ./brand1/brand1-services-peer.yml
